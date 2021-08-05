bd3e2bcdc8c623a2f77362c25ad8ae5854fef47a
IhsanFikri <ihsanfikri2@gmail.com>
2021-08-05 16:04:36 +0700

    listview already run

--- lib/home_screen/home_screen2.dart
+++ lib/home_screen/home_screen2.dart
@@ -2,26 +2,71 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
-class HomeScreen2 extends StatefulWidget {
-  const HomeScreen2({Key? key}) : super(key: key);
+class HomeScreenAPI extends StatefulWidget {
+  const HomeScreenAPI({Key? key}) : super(key: key);
 
   @override
-  _HomeScreen2State createState() => _HomeScreen2State();
+  _HomeScreenAPIState createState() => _HomeScreenAPIState();
 }
 
-class _HomeScreen2State extends State<HomeScreen2> {
-  late List animes;
-  void getData() async {
-    var response =
+late Future<List> animeList;
+late Future<List> resBody;
+
+class _HomeScreenAPIState extends State<HomeScreenAPI> {
+  Future<List> getAnime() async {
+    Response<Map> res =
         await Dio().get("https://api.jikan.moe/v3/top/anime/1/airing");
-    setState(() {
-      animes = response.data;
-    });
+
+    // resBody = res.data!['top'];
+    // animeList = resBody;
+
+    // print(resBody!.length);
+    // print(animeList[1]['image_url']);
+    return res.data!['top'];
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    animeList = getAnime();
+    //print(resBody);
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    print(animes);
-    return Scaffold();
+    // print(animeList);
+    print("hello");
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Cok2'),
+      ),
+      body: FutureBuilder(
+        future: animeList,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            // print(snapshot.data);
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    elevation: 10,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(snapshot.data[index]['title']),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+          return Text("kosong");
+        },
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // import 'package:bismillah1/home_screen/home_screen.dart';
 import 'package:bismillah1/home_screen/home_screen2.dart';
-import 'package:dio/dio.dart';
+// import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,95 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Test2(),
+      home: HomeScreenAPI(),
     );
   }
 }
-
-class Test2 extends StatelessWidget {
-  const Test2({Key? key}) : super(key: key);
-
-  void getAnime() async {
-    try {
-      Response<Map> res =
-          await Dio().get("https://api.jikan.moe/v3/top/anime/1/airing");
-      List? resBody = res.data!['top'];
-      print(resBody!.length);
-      print(resBody[1]['image_url']);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    getAnime();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello Cok2'),
-      ),
-    );
-  }
-}
-
-class Test extends StatelessWidget {
-  const Test({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Test'),
-      ),
-      body: GridView.count(
-        crossAxisCount: 1,
-        mainAxisSpacing: 10,
-        padding: const EdgeInsets.all(8),
-        children: [
-          GridTile(
-            child: Image.network(
-              'https://images.pexels.com/photos/3557818/pexels-photo-3557818.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-              fit: BoxFit.cover,
-            ),
-            footer: Material(
-              color: Colors.transparent,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(bottom: Radius.circular(4)),
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: GridTileBar(
-                backgroundColor: Colors.black45,
-                title: Text('TITLE'),
-                subtitle: Text('Subtitle'),
-              ),
-            ),
-          ),
-          GridTile(
-            header: Material(
-              color: Colors.transparent,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(4)),
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: GridTileBar(
-                title: Text('TITLE 2'),
-                subtitle: Text('Subtitle'),
-                backgroundColor: Colors.black54,
-              ),
-            ),
-            child: Image.network(
-              'https://images.pexels.com/photos/3557818/pexels-photo-3557818.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-              fit: BoxFit.cover,
-            ),
-          ),
-          GridTile(
-            child: Image.network(
-                'https://images.pexels.com/photos/3557818/pexels-photo-3557818.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class DataAnime {}
